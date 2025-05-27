28e1c0b2014c85f864c739eaa2a5a81ef905ad8a
Oliseh <studyug@hotmail.com>
2025-05-27 07:36:22 +0300

    fixed host issues

--- v4.2/src/utils/clients.ts
+++ v4.2/src/utils/clients.ts
@@ -1,21 +1,23 @@
 //make public and wallet clients
 
 import { createPublicClient, createWalletClient, http } from 'viem';
-import { celoAlfajores } from 'viem/chains';
+import { celoAlfajores, celo } from 'viem/chains';
+
+const isTestnet = import.meta.env.VITE_ENV === 'testnet';
 
 
 // const celoAlfajoresRpcUrl = process.env.NEXT_PUBLIC_CELO_ALFAJORES_RPC_URL;
 
-
+const rpcUrl = isTestnet ? "https://celo-alfajores.drpc.org" : "https://celo.drpc.org";
 const publicClient = createPublicClient({
-  chain: celoAlfajores,
-  transport: http(),
+  chain: isTestnet ? celoAlfajores : celo,
+  transport: http(rpcUrl),
 });
 
 const walletClient = createWalletClient({
-  chain: celoAlfajores,
+  chain: isTestnet ? celoAlfajores : celo,
   //set chain rpc url to https://celo-alfajores.drpc.org
-  transport: http(),
+  transport: http(rpcUrl),
 });
 
 
--- v4.2/vite.config.ts
+++ v4.2/vite.config.ts
@@ -18,6 +18,9 @@ export default defineConfig({
     }
   },
   base: '/',
+  server: {
+    allowedHosts: true
+  },
   preview: {
     allowedHosts: true
   }
